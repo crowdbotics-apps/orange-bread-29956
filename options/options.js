decd43a418bb906e9a31160a0f5a472b45a49857
Crowdbotics <team@crowdbotics.com>
2021-08-19 21:14:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orange_bread_29956Identifier",
+  url: "https://orange_bread_29956Identifier.botics.co",
+  api: "https://orange_bread_29956Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
